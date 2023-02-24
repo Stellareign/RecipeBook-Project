a367009f68dd549dce373beeca622e7f84b6084e
Stellareign <tuguran@mail.ru>
2023-02-24 13:34:36 +0300

    start

---
+++ src/test/java/me/ruana/recipeBook/BugetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ruana.recipeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
